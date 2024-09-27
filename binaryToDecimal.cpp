3b7cd6c090e33343f4ff65266a82df7a837fcea9
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2024-09-27 18:15:47 +0530

    Create binaryToDecimal.cpp

---
+++ binaryToDecimal.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int binaryToDecimal(int n) {
+    int ans=0;
+    int pow=1;
+    while(n!=0) {
+        int rem=n%10;
+        ans=ans+rem*pow;
+        n=n/10;
+        pow=pow*2; 
+    }
+    return ans;
+}
+
+int main(){
+   int n;
+   cout<<"enter the number: ";
+   cin>>n;
+   cout<<binaryToDecimal(n);
+    
+}
