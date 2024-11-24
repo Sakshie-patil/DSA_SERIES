d3e26bbf4b70e1bcd240b20d27dfe917a261732b
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2024-11-24 09:19:48 +0530

    Update MaxSubarraySum(kdanesAlgo).cpp

--- MaxSubarraySum(kdanesAlgo).cpp
+++ MaxSubarraySum(kdanesAlgo).cpp
@@ -1,3 +1,5 @@
+// by using array
+
 #include <iostream>
 #include <vector>
 #include<climits>
@@ -22,4 +24,32 @@ int main() {
     int size=sizeof(arr)/sizeof(int);
     int value=MaxSubarraySum(arr , size);
     cout<<value;
+}
+
+// vector using vector 
+
+#include <iostream>
+#include <vector>
+#include<climits>
+using namespace std;
+
+int MaxSubarraySum(vector <int>& vect , int size) {
+    int maxsum=INT_MIN;
+    int currsum=0;
+    for(int i : vect) {
+        currsum= currsum + i;
+        maxsum = max(currsum , maxsum);
+        if ( currsum <0) {
+            currsum=0;
+        }
+    }
+    return maxsum;
+}
+
+int main () {
+    vector <int> vect = { 3 , -4 ,5,4,-1,7,-8};
+    int size=vect.size();
+   int value= MaxSubarraySum(vect , size);
+   cout<<value;
+    
 }
