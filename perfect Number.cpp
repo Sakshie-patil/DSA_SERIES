35745470552a3deb2aa44d69bb36884c3c5b806a
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2025-07-15 12:27:32 +0530

    Create perfect Number.cpp

---
+++ perfect Number.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int isPerfect(int n) {
+    int sum=0;
+  for(int i=1;i<=n/2;i++) {
+      if(n%i == 0) {
+          sum+= i;
+      }
+  }
+  return sum==n;
+}
+
+int main() {
+       int n = 6;
+    if (isPerfect(n))
+        cout << n << " is a Perfect Number.";
+    else
+        cout << n << " is NOT a Perfect Number.";
+
+}
