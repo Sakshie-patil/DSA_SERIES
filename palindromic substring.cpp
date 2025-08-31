847fc25d0c8eb16b45a27166e1bc8f997b2e1319
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2025-08-31 12:23:46 +0530

    Update palindromic substring.cpp

--- palindromic substring.cpp
+++ palindromic substring.cpp
@@ -22,3 +22,31 @@ bool palindrome(string &s , int left , int right) {
         }
         return count;
     }
+
+
+
+ int countSubstrings(string s) {
+        int count = 0;
+        int n = s.size();
+        for(int i=0;i<n;i++) {
+            // odd
+            int l=i;
+            int r=i;
+            while(l >= 0 && r <= n && s[l] == s[r]) {
+                count++;
+                l--;
+                r++;
+            }
+
+            // even
+            l=i;
+            r=i+1;
+              while(l >= 0 && r <= n && s[l] == s[r]) {
+                count++;
+                l--;
+                r++;
+            }
+            
+        }
+        return count;
+    }
