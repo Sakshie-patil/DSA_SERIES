341b3ecc4e171cac2780c393400d55e30a4c0654
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2026-07-10 13:30:36 +0530

    Clean up star pyramid pattern code
    Removed unused character variable and commented out inverted pyramid code.

---
+++ star_pyramid_pattern.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter number: ";
+    cin >> n;
+
+ // inverted pyramid
+    // rows
+    // for(int i=1; i<=n;i++) {
+    //       int num = 1;
+    
+    //       // spaces
+    //       for(int j=1;j<=i-1;j++) {
+    //           cout<<"  ";
+    //       }
+    //     // what to print
+    //     for(int j=2*(n-i)+1;j>=1;j--) {
+    //         cout<<"*"<<" ";
+    //         // num++;
+    //     }
+    //       cout<<endl;
+    //     }
+
+
+  // straight pyramid
+    for(int i=1;i<=n;i++) {
+        // spaces
+        for(int j=1;j<=n-i;j++) {
+            cout<<"  ";
+        }
+        
+        // stars
+        for(int j=1;j<=2*i-1;j++) {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
+    
+    
+    
+    
