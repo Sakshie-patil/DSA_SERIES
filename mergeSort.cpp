e4e095a90364b16a7965fb831dc564b02628aa35
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2026-07-12 09:37:21 +0530

    Refactor merge sort implementation to recursive version

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,43 +1,56 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <vector>
 using namespace std;
 
-void merge(vector<int>& arr, int st, int mid, int end) {
-    vector<int> temp;
-    int i = st, j = mid + 1;
-
-    while (i <= mid && j <= end) {
-        if (arr[i] <= arr[j]) temp.push_back(arr[i++]);
-        else temp.push_back(arr[j++]);
+void merge(vector<int>&arr, int low, int mid, int high) {
+    vector<int>temp;
+    
+    int i=low;
+    int j=mid+1;
+    
+    while(i <=mid && j <= high) {
+        if(arr[i] <= arr[j]) {
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else {
+            temp.push_back(arr[j]);
+            j++;
+        }
     }
-    while (i <= mid) temp.push_back(arr[i++]);
-    while (j <= end) temp.push_back(arr[j++]);
-
-    for (int k = 0; k < temp.size(); k++) {
-        arr[st + k] = temp[k];
+    
+    while(i <= mid) {
+         temp.push_back(arr[i]);
+         i++;
+    }
+    
+    while(j <= high) {
+        temp.push_back(arr[j]);
+        j++;
+    }
+    
+    // remember i value
+    for(int i=low;i<=high;i++) {
+        arr[i] = temp[i-low];
     }
 }
 
-void mergeSortIterative(vector<int>& arr) {
-    int n = arr.size();
-
-    // Current size of subarrays to merge (1, 2, 4, 8...)
-    for (int size = 1; size < n; size *= 2) {
-        // Pick starting index of left subarray
-        for (int left = 0; left < n - size; left += 2 * size) {
-            int mid = left + size - 1;
-            int right = min(left + 2 * size - 1, n - 1);
-
-            merge(arr, left, mid, right);
-        }
-    }
+void mergeSort(vector<int>&arr, int low, int high) {
+    //*****IMP*****
+    if(low>=high) return;
+    int mid = (low + high)/2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
+    merge(arr,low,mid,high);
 }
 
 int main() {
-    vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
-    mergeSortIterative(arr);
-
-    cout << "Sorted Array: ";
-    for (int x : arr) cout << x << " ";
-    cout << endl;
+   vector<int>arr = {100,90,80,70,60,50};
+   int n = arr.size();
+  mergeSort(arr,0,n-1);
+   for(int i=0;i<n;i++) {
+       cout<<arr[i]<<" ";
+   }
+    return 0;
 }
