c6a7c2566593fda4abe3f3eabf75bc0584d1aa7a
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2026-07-12 09:05:53 +0530

    Refactor insertionSort to use vector instead of array

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,29 +1,30 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void insertionSort(int arr[] , int size) {
-    for(int i=1; i<size ;i++) {  /*array is starting from unsorted part i.e. arr[1] */
-    int curr=arr[i];
-    int prev = i-1;
-    while(arr[prev]>curr && prev>=0) {
-        arr[prev+1] = arr[prev];
-        prev--;
-    }
-    arr[prev+1] = curr; 
+void insertionSort(vector<int>&arr, int n) {
+    // insertion sort
+    for(int i=1;i<n;i++) {
+        int key = arr[i];
+        int j = i-1;
         
-    }
-}
-
-
-void printArray(int arr[],int size) {
-    for(int i=0;i<size;i++) {
-        cout<<arr[i]<<" ";
+        while(j>=0 && arr[j] > key) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
     }
 }
 
 int main() {
-    int arr[] = {66,5,4,3,2,1,7,8};
-    int size=sizeof(arr)/sizeof(int);
-    insertionSort(arr , size);
-    printArray(arr,size);
+   vector<int>arr = {100,90,80,70,60,50};
+   // 100 90 80 70 60 50
+   // 
+   int n = arr.size();
+  insertionSort(arr,n);
+   for(int i=0;i<n;i++) {
+       cout<<arr[i]<<" ";
+   }
+    return 0;
 }
